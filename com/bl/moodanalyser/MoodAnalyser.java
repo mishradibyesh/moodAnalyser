03d14229759b8be8bd726a1ead50bcde6c24bfee
mishradibyesh <dibyeshmishra207@gmail.com>
2021-11-08 00:07:23 +0530

    ADDED : repeat code to test sad mood using constructor

--- com/bl/moodanalyser/MoodAnalyser.java
+++ com/bl/moodanalyser/MoodAnalyser.java
@@ -3,32 +3,32 @@ package com.bl.moodanalyser;
 public class MoodAnalyser {
 
 	//method to analyse mood by reading string
-	private String message;
+	 private String message;
 
-	public MoodAnalyser(String message) {
-		this.message = message;
+	    public MoodAnalyser(String message) {
+	        this.message = message;
+	    }
+
+	    public MoodAnalyser() {
+
+	    }
+
+	    public String getMessage() {
+	        return message;
+	    }
+
+	    public void setMessage(String message) {
+	        this.message = message;
+	    }
+
+	    public String analyseMood() {
+
+	        if (message.toLowerCase().contains("sad")) {
+	            return "SAD";
+	        } else if (message.toLowerCase().contains("any")) {
+	            return "HAPPY";
+	        } else {
+	            return null;
+	        }
+	    }
 	}
-
-	public MoodAnalyser() {
-
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public String analyseMood(String message) {
-
-		if (message.toLowerCase().contains("sad")) {
-			return "SAD";
-		} else if (message.toLowerCase().contains("any")) {
-			return "HAPPY";
-		} else {
-			return null;
-		}
-	}
-}
--- com/bl/moodanalyser/MoodAnalyserTest.java
+++ com/bl/moodanalyser/MoodAnalyserTest.java
@@ -9,15 +9,10 @@ public class MoodAnalyserTest {
 
 	MoodAnalyser moodAnalyser = new MoodAnalyser();
 
-	 @Test
-	    public void givenMessage_IsProper_ShouldReturnSad() {
-	        String actualResult = moodAnalyser.analyseMood("I am Sad in Mood");
-	        Assert.assertEquals("SAD", actualResult);
-	    }
-
-	    @Test
-	    public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
-	        String actualResult = moodAnalyser.analyseMood("I am in Any Mood");
-	        Assert.assertEquals("HAPPY", actualResult);
-	    }
+	@Test
+	public void givenMessage_IsProper_ShouldReturnSad() {
+		moodAnalyser.setMessage("I am in Sad Mood");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("SAD", actualResult);
 	}
+}
