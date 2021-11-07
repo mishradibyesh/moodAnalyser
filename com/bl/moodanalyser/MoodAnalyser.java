666f89e26654b54aaf81a4779ce59329bbee9e5a
mishradibyesh <dibyeshmishra207@gmail.com>
2021-11-07 23:08:33 +0530

    ADDED : code to analyse happ or sad mood

---
+++ com/bl/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,24 @@
+package com.bl.moodanalyser;
+
+public class MoodAnalyser {
+
+	//method to analyse mood by reading string
+	public String analyseMood(String message) {
+		if (message.toLowerCase().contains("happy")) {
+			return "Happy";
+		} else if (message.toLowerCase().contains("sad")) {
+			return "Sad";
+		} else {
+			return null;
+		}
+	}
+
+	//main method
+	public static void main(String[] args) {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMood("I am Happy");
+		System.out.println(mood);
+		mood = moodAnalyser.analyseMood("I am Sad");
+		System.out.println(mood);
+	}
+}
