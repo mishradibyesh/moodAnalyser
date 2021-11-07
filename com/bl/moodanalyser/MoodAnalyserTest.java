2e0600e6d85e5bd5c5c9de2b8c0f259dab36b9f3
mishradibyesh <dibyeshmishra207@gmail.com>
2021-11-08 00:14:58 +0530

    ADDED : repeat code to test happy mood using constructor

--- com/bl/moodanalyser/MoodAnalyserTest.java
+++ com/bl/moodanalyser/MoodAnalyserTest.java
@@ -8,11 +8,18 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
 	MoodAnalyser moodAnalyser = new MoodAnalyser();
+	
+    @Test
+    public void givenMessage_IsProper_ShouldReturnSad() {
+        moodAnalyser.setMessage("I am in Sad Mood");
+        String actualResult = moodAnalyser.analyseMood();
+        Assert.assertEquals("SAD", actualResult);
+    }
 
-	@Test
-	public void givenMessage_IsProper_ShouldReturnSad() {
-		moodAnalyser.setMessage("I am in Sad Mood");
-		String actualResult = moodAnalyser.analyseMood();
-		Assert.assertEquals("SAD", actualResult);
-	}
+    @Test
+    public void givenMessage_IsProper_AndContain_Any_ShouldReturnHappy() {
+        moodAnalyser.setMessage("I am in Any Mood ");
+        String actualResult = moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", actualResult);
+    }
 }
